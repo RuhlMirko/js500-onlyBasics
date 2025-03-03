9a4abcdd4dbe342c7146acdaa69cfe613fdddf35
Mirko <ruhlmirkojoel@gmail.com>
2025-03-03 15:56:58 -0300

    Testing ternary operators

--- js-intro/1 - Introduction/app.js
+++ js-intro/1 - Introduction/app.js
@@ -6,7 +6,7 @@ const admin = adminName;
 prompt("Type something: ", "");
 let userInput = confirm("Accept or decline");
 console.log(userInput);
-*/
+
 let a = +prompt("First number?", 1);
 let b = +prompt("Second number?", 2);
 
@@ -14,3 +14,15 @@ alert(a + b); // 12
 
 console.log("new test");
 console.log("Newer test");
+
+
+const userInput = prompt("Enter a number");
+
+userInput > 0
+  ? alert(1)
+  : userInput < 0
+  ? alert(-1)
+  : userInput == 0
+  ? alert(0)
+  : alert(null);
+*/
