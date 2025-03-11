2a8277731b4e50eb1cb20fdce803e19df7c1c941
Mirko <ruhlmirkojoel@gmail.com>
2025-03-11 11:25:54 -0300

    Added day 3 project

---
+++ 30-day-challenge/day3/app.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  return {
+    toBe: (val2) => {
+      return (
+        val2 === val ||
+        (() => {
+          throw new Error("Not Equal");
+        })()
+      );
+    },
+    notToBe: (val2) => {
+      return (
+        val2 !== val ||
+        (() => {
+          throw new Error("Equal");
+        })()
+      );
+    },
+  };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
