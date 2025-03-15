201089409263c2aa46d303257e02664e5122490d
Mirko <ruhlmirkojoel@gmail.com>
2025-03-15 13:26:25 -0300

    Final version

--- 30-day-challenge/day6/app.js
+++ 30-day-challenge/day6/app.js
@@ -1,18 +1,28 @@
-"use strict";
-
 /**
  * @param {number[]} arr
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function (arr, fn) {};
+var map = function (arr, fn) {
+  const res = [];
+  for (const ind in arr) {
+    res.push(fn(arr[ind], Number(ind)));
+  }
+  return res;
+};
 
-console.log(map([1, 2, 3]), function plusone(n) {
-  return n + 1;
-});
-console.log(map([1, 2, 3]), function plusInd(n, i) {
-  return n + i;
-});
-console.log(map([1, 2, 3]), function constant(n, i) {
-  return 42;
-});
+console.log(
+  map([1, 2, 3], function plusone(n) {
+    return n + 1;
+  })
+);
+console.log(
+  map([1, 2, 3], function plusInd(n, i) {
+    return n + i;
+  })
+);
+console.log(
+  map([1, 2, 3], function constant(n, i) {
+    return 42;
+  })
+);
