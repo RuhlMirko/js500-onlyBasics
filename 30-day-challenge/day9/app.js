8ca8a3b7f43bdd91ccd21863ddb67bf9c3ebab9e
Mirko <ruhlmirkojoel@gmail.com>
2025-03-18 01:27:24 -0300

    Completed day 9

---
+++ 30-day-challenge/day9/app.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+console.log(argumentsLength(5));
+console.log(argumentsLength({}, null, "3"));
